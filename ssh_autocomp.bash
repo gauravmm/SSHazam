26b48811c35b4ff8e782eca13b6a5daec4daf3b1
Gaurav Manek <gaurav@gauravmanek.com>
2019-04-10 13:34:15 -0400

    Refactor

--- ssh_autocomp.bash
+++ ssh_autocomp.bash
@@ -6,31 +6,62 @@ MAX_CM_OPEN=1
 DEBUG=""
 
 # If there are more than MAX_CM_OPEN connections possible, use this heuristic to select the servers to connect to:
-HEURISTIC="last-n-conn"
+HEURISTIC="last" # "" "last" "most"
 HEURISTIC_CACHE_LNC=~/.ssh_last_n
+HEURISTIC_CACHE_MC=~/.ssh_most
 
 dbgecho() {
     # Comment these out to disable printing debug messages:
     [ ! -z "$DEBUG" ] && echo "$@" >> debug.log
 }
 
-function __maybe_make_cache_lnc() {
-    # TODO: Run this if the cache is old or if the SSH config file is newer than the cache.
+function __maybe_make_cache() {
+    local CACHE_FILE
+    CACHE_FILE="$1"
 
     local TS_CACHE TS_CONFIG CFG TS_CFG
 
-    TS_CACHE=$(stat -c %Y $HEURISTIC_CACHE_LNC)
+    TS_CACHE=$(stat -c %Y "$CACHE_FILE")
     TS_CONFIG=$(date -d "-1 day" +%s) # One day ago; this way we renew the cache daily
     for CFG in $SSH_CONFIG_PATHS; do
         TS_CFG=$(stat -c %Y $CFG)
         (( $TS_CFG > $TS_CONFIG )) && TS_CONFIG=$TS_CFG
     done
 
-    if [ $TS_CONFIG -gt $TS_CACHE ]; then
-        __make_cache_lnc "$@"
-    fi
+    return [ $TS_CONFIG -gt $TS_CACHE ];
 }
 
+# Selected Connections
+OPEN_CONN=()
+function __select_conn() {
+    local HEURISTIC_FILE CANDIDATES
+    HEURISTIC_FILE="$1"
+    shift
+    CANDIDATES=( "$@" )
+
+    local LNC TARGET
+    # Read in the cache
+    # Store in $OPEN_CONN the first $MAX_CM_OPEN lines of $HEURISTIC_FILE that appear in $CANDIDATES
+    OPEN_CONN=()
+    while read LNC || [[ -n "$LNC" ]]; do
+        for TARGET in ${CANDIDATES[@]}; do
+            if [ ! -z "$TARGET" ] && [ $LNC = $TARGET ]; then
+                dbgecho "LNC: $TARGET"
+                NEW_CANDIDATES+=($TARGET)
+                CANDIDATES=("${CANDIDATES[@]/$TARGET}")
+                break
+            fi
+        done
+
+        if [ "${#OPEN_CONN[@]}" -ge "$MAX_CM_OPEN" ]; then
+            break
+        fi
+    done < "$HEURISTIC_FILE"
+}
+
+#
+# MOST-CONNECTED
+#
 function __make_cache_lnc() {
     local HOSTS
     HOSTS=("$@")
@@ -67,6 +98,49 @@ function __make_cache_lnc() {
 
 }
 
+
+#
+# LAST-N-CONNECTIONS
+#
+function __make_cache_lnc() {
+    local HOSTS
+    HOSTS=("$@")
+
+    (
+        if flock -n 199; then
+            local ORDERED
+            ORDERED=()
+            for LINE in $(fc -rl -3000 | grep '^[^a-zA-Z]*ssh'); do
+                # dbgecho "SEARCHING FOR ${HOST[@]} IN $LINE"
+                for HOST in "${HOSTS[@]}"; do
+                    if [ ! -z "$HOST" ] && [[ $LINE == *$HOST* ]]; then
+                        dbgecho "ORDERED B: $HOST"
+                        ORDERED+=($HOST)
+                        HOSTS=("${HOSTS[@]/$HOST}")
+                        break
+                    fi
+                done
+
+                if [ "${#HOSTS[@]}" = 0 ]; then
+                    break
+                fi
+            done
+
+            # Write output to cache:
+            printf "%s\n" "${ORDERED[@]}" > "$HEURISTIC_CACHE_LNC"
+            dbgecho "ORDERED: ${ORDERED[@]}"
+
+            rm "$HEURISTIC_CACHE_LNC.lock"
+        else
+            dbgecho "LNC :: CANNOT ACQUIRE CACHE LOCK"
+        fi
+    ) 199>"$HEURISTIC_CACHE_LNC.lock"
+
+}
+
+#
+# MAIN
+#
 function __open_conn() {
     local SRVSTR CMPATH
     SRVSTR="$2@$1"
@@ -131,46 +205,38 @@ function _ssh()
     else
         COMPREPLY=(${TARGETS[@]/#/$LAST_USER@})
     fi
-    # Now COMPREPLY contains the list of matching hosts.
-
     dbgecho "COMPREPLY ${#COMPREPLY[@]}: ${COMPREPLY[@]/#/-> }"
+    # Now COMPREPLY contains the list of matching hosts.
 
     if [ "${#TARGETS[@]}" -gt $MAX_CM_OPEN ]; then
         dbgecho "TOO MANY TARGETS: MAX_CM_OPEN: $MAX_CM_OPEN"
-        if [ "$HEURISTIC" = "last-n-conn" ]; then
-            __maybe_make_cache_lnc ${HOSTS[@]}
-            # Read in the cache
-            local NEW_TARGETS
-            NEW_TARGETS=()
-            # Store in $NEW_TARGETS the first $MAX_CM_OPEN lines of $HEURISTIC_CACHE_LNC that appear in $TARGETS
-            while read LNC || [[ -n "$LNC" ]]; do
-                for TARGET in ${TARGETS[@]}; do
-                    if [ ! -z "$TARGET" ] && [ $LNC = $TARGET ]; then
-                        dbgecho "LNC: $TARGET"
-                        NEW_TARGETS+=($TARGET)
-                        TARGETS=("${TARGETS[@]/$TARGET}")
-                        break
-                    fi
-                done
+        local NEW_TARGETS
+        NEW_TARGETS=()
 
-                if [ "${#NEW_TARGETS[@]}" -ge "$MAX_CM_OPEN" ]; then
-                    break
-                fi
-            done < "$HEURISTIC_CACHE_LNC"
+        if [ "$HEURISTIC" = "last" ]; then
+            if __maybe_make_cache "$HEURISTIC_CACHE_LNC"; then
+                __make_cache_lnc ${HOSTS[@]}
+            fi
+            __select_conn "$HEURISTIC_CACHE_LNC" ${TARGETS[@]}
 
-            # Update targets:
-            TARGETS=(${NEW_TARGETS[@]})
-            dbgecho "TARGETS [LNC]: ${NEW_TARGETS[@]}"
+        if [ "$HEURISTIC" = "most" ]; then
+            if __maybe_make_cache "$HEURISTIC_CACHE_MC"; then
+                __make_cache_mc ${HOSTS[@]}
+            fi
+            __select_conn "$HEURISTIC_CACHE_MC" ${TARGETS[@]}
 
         else
             dbgecho "NO HEURISTIC; ABANDONING ATTEMPT"
             return 128
         fi
+    else
+        # Open all connections:
+        OPEN_CONN=( ${TARGETS[@]} )
     fi
 
-    dbgecho "TARGETS: ${TARGETS[@]}"
-    for TARGET in "${TARGETS[@]}"; do
-        __open_conn "$TARGET" $LAST_USER
+    dbgecho "OPEN_CONN: ${OPEN_CONN[@]}"
+    for TARGET in "${OPEN_CONN[@]}"; do
+        __open_conn "$TARGET" "$LAST_USER"
     done
 
     return 0
